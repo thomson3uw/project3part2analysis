c143c6d4a5017654d1a164be8d21216772fc7686
Alexander Thomson <athomson@Alexanders-MacBook-Pro.local>
2021-06-09 17:45:13 -0700

    Added the specified workflow to the analysis file

--- Code/my_rf_cv.R
+++ Code/my_rf_cv.R
@@ -8,8 +8,6 @@
 #'
 #' @return The average MSE across all k folds.
 #'
-#' @importFrom stats model.frame model.matrix model.response predict pt sd na.omit
-#'
 #' @export
 my_rf_cv <- function(k) {
   if (k <= 1) {
@@ -19,7 +17,7 @@ my_rf_cv <- function(k) {
     stop("k must be an integer greater than or equal to 1")
   }
   # omit the NAs in my_penguins
-  penguins_no_NA <- na.omit(project3part1package::my_penguins)
+  penguins_no_NA <- na.omit(my_penguins)
   # set the relevant columns of penguins as our data
   data <- data.frame("body_mass" = penguins_no_NA$body_mass_g,
                      "bill_length" = penguins_no_NA$bill_length_mm,
@@ -34,10 +32,10 @@ my_rf_cv <- function(k) {
   MSEs <- c()
   # loop through each fold
   for (i in 1:k) {
-    data_train <- data %>% dplyr::filter(fold != i)
-    data_test <- data %>% dplyr::filter(fold == i)
+    data_train <- data %>% filter(fold != i)
+    data_test <- data %>% filter(fold == i)
     
-    rf_cv <- randomForest::randomForest(body_mass ~ bill_length + bill_depth + flipper_length,
+    rf_cv <- randomForest(body_mass ~ bill_length + bill_depth + flipper_length,
                                         data = data_train, ntree = 100)
     
     rf_pred <- predict(rf_cv, data_test[, -1])
